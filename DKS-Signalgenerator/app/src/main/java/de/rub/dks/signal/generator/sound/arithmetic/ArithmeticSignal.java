dbbc2d14913b551074330c81858a66cf37edbb93
Max <max-hoffmann@gmx.de>
2015-03-19 18:39:48 +0100

    Update ArithmeticSignal.java
    Dokumentation hinzugefügt

--- DKS-Signalgenerator/app/src/main/java/de/rub/dks/signal/generator/sound/arithmetic/ArithmeticSignal.java
+++ DKS-Signalgenerator/app/src/main/java/de/rub/dks/signal/generator/sound/arithmetic/ArithmeticSignal.java
@@ -4,6 +4,10 @@ import com.jjoe64.graphview.GraphView.GraphViewData;
 
 import de.rub.dks.signal.generator.sound.Signal;
 
+/*
+* Advanced Signal class for operations on two Signals.
+* Extend this class and implement the calculate method which is used by all the other methods.
+*/
 public abstract class ArithmeticSignal {
 	protected Signal a, b;
 	protected double audioPosA, audioPosB;
@@ -12,8 +16,20 @@ public abstract class ArithmeticSignal {
 		setOperands(aa, bb);
 	}
 
+	/**
+     	* Calculates the desired operation of the two operand's values at given frequency and phase
+     	* @param freq1  the first signal's frequency
+     	* @param phase1	the first signal's phase
+     	* @param freq2  the seconds signal's frequency
+     	* @param phase2	the seconds signal's phase
+     	*/
 	public abstract double calculate(double freq1, double phase1, double freq2, double phase2);
 
+	/**
+     	* Generates a GraphViewData array with Signal.GRAPH_SAMPLES entries.
+     	* This method uses the internal phase and frequency values of its operand members.
+     	* @return    	the samples
+     	*/
 	public GraphViewData[] getGraphData() {
 		GraphViewData[] data = new GraphViewData[Signal.GRAPH_SAMPLES];
 		double x = 0, y = 0;
@@ -27,6 +43,11 @@ public abstract class ArithmeticSignal {
 		return data;
 	}
 
+	/**
+     	* Generates a byte array with Signal.BUFFER_SIZE samples to be played back, resembling the sound of the signal.
+     	* This method uses the internal phase and frequency values of its operand members.
+     	* @return    	the samples
+     	*/
 	public byte[] getAudioBytes() {
 		byte[] samples = new byte[Signal.BUFFER_SIZE];
 		for (int i = 0; i < Signal.BUFFER_SIZE; i++) {
@@ -37,6 +58,8 @@ public abstract class ArithmeticSignal {
 		}
 		return samples;
 	}
+	
+	// Getters and setters for the internal variables
 
 	public void setOperands(Signal aa, Signal bb) {
 		a = aa;
