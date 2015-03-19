388b8b5e4b01a3cba732d35639ff2a1768f94c58
Max <max-hoffmann@gmx.de>
2015-03-19 18:33:23 +0100

    Update SinusSignal.java

--- DKS-Signalgenerator/app/src/main/java/de/rub/dks/signal/generator/sound/SinusSignal.java
+++ DKS-Signalgenerator/app/src/main/java/de/rub/dks/signal/generator/sound/SinusSignal.java
@@ -2,6 +2,7 @@ package de.rub.dks.signal.generator.sound;
 
 public class SinusSignal extends Signal {
 
+	@Override
 	public double calculate(double freq, double phase) {
 		return Math.sin(freq + phase);
 	}
